2f8cc0fc1a7f3aadb1fb0bb8ddeef8981d7ece56
agastyathoppur <79033188+agastyathoppur@users.noreply.github.com>
2021-04-23 21:13:25 +0530

    Update IAS.h
    Load and store added, struct instruction replaced with long int. Other load functions and jump will be added later

--- IAS.h
+++ IAS.h
@@ -1,26 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include<math.h>
-#define MAX_SIZE 1000
-#define SIZE_PC 10
-
-//Instruction format
-
-typedef struct instruction
+#include "IAS.h"
+void load(int instr_idx)   //Takes index of memory location of instruction to be loaded as a parameter
 {
-  int lhs;
-  int rhs;
-} instruction;
+    *(&ACC)=MainMemory[instr_idx];
+}
+void stor(int instr_idx)
+{
+    *(&MainMemory[instr_idx])=ACC;
+}
+void fetch(int value_at_pc)
+{
+    MAR = value_at_pc;
+    MBR = MainMemory[MAR]; //data_
+    int left = MBR>>20;
+    int right= MBR & 0b0000000000000000000011111111111111111111;
+    if(left==0)
+    {
+        //RHS
+        IR = right >> 12;
+        MAR = right & 0b00000000111111111111;
+    }
+    else
+    {
+        //LHS
+        if(left_flag==0)
+        {
+            IBR = MBR;
+            IR = left >> 12;
+            MAR = left & 0b00000000111111111111;
+        }
+        //RHS, if LHS is present and has completed its decode cycle
+        else
+        {
+            IR=right>>12;
+            MAR=right & 0b00000000111111111111;
+        }
+         
+    }
 
-//Registers
 
-instruction MainMemory[MAX_SIZE];          
-int PC[SIZE_PC];
-int MAR;
-int IR;
-int IBR;
-instruction MBR;
-int ACC;
-int MQ;
-int PC_index = 0;
-int left_flag = 0;          //If we have a single instruction _> by default its right instruction so 0 
+    
+
+}
+
+void decode(int IR, int MAR)
+{
+    switch (IR)
+    {
+        //load-> abs , normal , -
+    }   //stor
+        //artih + - * as + division
+}       //factorial
+        //Jumps + conditional jumps
