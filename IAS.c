96ab3ccf59cac6dd39fa09f773ef64a58e75d105
agastyathoppur <79033188+agastyathoppur@users.noreply.github.com>
2021-04-23 21:14:57 +0530

    Update IAS.c

--- IAS.c
+++ IAS.c
@@ -1,25 +1,45 @@
-#include <IAS.h>
-
+#include "IAS.h"
+void load(int instr_idx)   //Takes index of memory location of instruction to be loaded as a parameter
+{
+    *(&ACC)=MainMemory[instr_idx];
+}
+void stor(int instr_idx)
+{
+    *(&MainMemory[instr_idx])=ACC;
+}
 void fetch(int value_at_pc)
 {
     MAR = value_at_pc;
     MBR = MainMemory[MAR]; //data_
-
-    IBR = MBR.rhs;
-    IR = (MBR.lhs) >> 12;
-    MAR = ((MBR.lhs)) & 0b00000000111111111111;
-    PC_index++;
-
-    if (left_flag == 0)
+    int left = MBR>>20;
+    int right= MBR & 0b0000000000000000000011111111111111111111;
+    if(left==0)
     {
-        decode(IR, MAR);
-        //decode on right
+        //RHS
+        IR = right >> 12;
+        MAR = right & 0b00000000111111111111;
     }
     else
     {
-        //decode left
-        //decode right
+        //LHS
+        if(left_flag==0)
+        {
+            IBR = MBR;
+            IR = left >> 12;
+            MAR = left & 0b00000000111111111111;
+        }
+        //RHS, if LHS is present and has completed its decode cycle
+        else
+        {
+            IR=right>>12;
+            MAR=right & 0b00000000111111111111;
+        }
+         
     }
+
+
+    
+
 }
 
 void decode(int IR, int MAR)
